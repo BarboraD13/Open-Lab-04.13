b82a22df825a44635fd858093735f629be56eaca
BarboraD13 <71934521+BarboraD13@users.noreply.github.com>
2020-10-30 11:14:51 +0100

    ++

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._13
 {
@@ -6,7 +6,23 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int[,] i = new int[str1.Length + 1, str2.Length + 1];
+            int a = 0;    
+            int b = 0;    
+
+            for (var v = 0; v < str1.Length; v++)
+                for (var r = 0; r < str2.Length; r++)
+                    if (str1[v] == str2[r])
+                    {
+                        int j = i[v + 1, r + 1] = i[v, r] + 1;
+                        if (j > i[a, b])
+                        {
+                            a = v + 1;
+                            b = r + 1;
+                        }
+                    }
+
+            return str1.Substring(a - i[a, b], i[a, b]);
         }
     }
 }
